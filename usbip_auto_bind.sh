2141b71d07d0e8a9901e9a57e268c245e400bf73
Serebrennikov Sergey <Smit-bb@mail.ru>
2022-06-14 22:34:09 +0500

    made a base for sorting and bind usb

--- usbip_auto_bind.sh
+++ usbip_auto_bind.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+allowed_vid_pid=()
+
+while :; do
+    for allowed_id in ${allowed_vid_pid[@]}; do
+        local_vid_pid=$()
+        for local_id in $local_vid_pid; do
+            usbip bind -b $local_id 2>/dev/null
+        done
+    done
+    sleep 2
+done
