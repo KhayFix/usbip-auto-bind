85c3478bfe3967c0842266adfd54a1841076100d
Serebrennikov Sergey <Smit-bb@mail.ru>
2022-06-14 23:16:17 +0500

    wrote the basic sort command vid pid

--- usbip_auto_bind.sh
+++ usbip_auto_bind.sh
@@ -4,7 +4,7 @@ allowed_vid_pid=()
 
 while :; do
     for allowed_id in ${allowed_vid_pid[@]}; do
-        local_vid_pid=$()
+        local_vid_pid=$(usbip list -p -l | awk "/${allowed_id}/" | cut -d'#' -f1 | cut -d'=' -f2)
         for local_id in $local_vid_pid; do
             usbip bind -b $local_id 2>/dev/null
         done
