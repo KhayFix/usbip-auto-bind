42c9fb7079d383311c90eabd3417a5a4c3cd8a03
Serebrennikov Sergey <Smit-bb@mail.ru>
2022-06-15 21:57:21 +0500

    wrote a script to unbind all usb

--- usbip_auto_unbind.sh
+++ usbip_auto_unbind.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+PATH=/etc:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin
+
+process_list=$(ps -aux | grep 'usbip_auto_bind.sh' | grep -v 'grep' | awk -F' ' '{print $2}')
+[ -z "$process_list" ] || kill "$process_list"
+
+local_vid_pid=$(usbip list -p -l | cut -d'#' -f1 | cut -d'=' -f2)
+for local_id in $local_vid_pid; do
+    usbip unbind -b $local_id 2>/dev/null
+done
